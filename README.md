6a7b5b6132bbe73fbcd390548319d30265869e5d
Mohsen Shabanian <mohsen1299@gmail.com>
2019-11-03 01:05:21 +0330

    problem in make block array

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Block Chain with C++
 
 idea from forud video
+
+```sh
+g++ main.cpp -o main && ./main
+```
---
+++ blockchain.h
@@ -0,0 +1,15 @@
+// #include "block.h"
+
+// class Blockchain
+// {
+//     public:
+//         Block *blocks;
+
+//         // Blockchain();
+// };
+
+// Blockchain::Blockchain()
+// {
+//     Block first("Genesis",0);
+//     blocks[0] = first;
+// }
